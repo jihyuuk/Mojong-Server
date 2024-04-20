3343691ccec4a0ddc1608e01a24ab162626299b3
jihyuuk_home <jihyuk.dev@gmail.com>
2024-04-21 05:34:54 +0900

    Build: 최종 빌드전 수정

--- src/main/java/com/example/mojong/controller/ItemController.java
+++ src/main/java/com/example/mojong/controller/ItemController.java
@@ -1,22 +1,22 @@
-package com.example.mojong.controller;
-
-import com.example.mojong.InIt;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
-
-@Slf4j
-@RestController
-@RequiredArgsConstructor
-public class ItemController {
-
-    private final InIt inIt;
-
-    @PostConstruct
-    public void run(){
-        inIt.init();
-    }
-
-
-}
+//package com.example.mojong.controller;
+//
+//import com.example.mojong.InIt;
+//import jakarta.annotation.PostConstruct;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//public class ItemController {
+//
+//    private final InIt inIt;
+//
+//    @PostConstruct
+//    public void run(){
+//        inIt.init();
+//    }
+//
+//
+//}
