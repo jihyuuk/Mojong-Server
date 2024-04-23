ecae2f79bdc8a83b44ecca82a58e3d1f09a727e7
jihyuuk_home <jihyuk.dev@gmail.com>
2024-04-23 23:20:57 +0900

    BugFIx: 서버 타임존 Asia/Seoul 적용함

--- src/main/java/com/example/mojong/MojongApplication.java
+++ src/main/java/com/example/mojong/MojongApplication.java
@@ -1,13 +1,21 @@
 package com.example.mojong;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MojongApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MojongApplication.class, args);
+	}
+
+	@PostConstruct
+	public void settingTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
